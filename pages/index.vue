5525ccc1c4d80a8d935a08c0cbc7cd5dfc824b7e
keylaazahrap2020@gmail.com <keylaazahrap2020@gmail.com>
2023-11-23 14:37:54 +0700

    push tugas ke 6

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <Splide :options="options" aria-label="My Favorite Images">
+    <SplideSlide>
+      <img src="image1.jpg" alt="Sample 1">
+    </SplideSlide>
+    <SplideSlide>
+      <img src="../pages" alt="Sample 2">
+    </SplideSlide>
+  </Splide>
+</template>
+
+
+<script>
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import { defineComponent } from 'vue';
+
+
+export default defineComponent( {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+
+
+  setup() {
+    const options = {
+      rewind: true,
+      gap   : '1rem',
+    };
+
+
+    return { options };
+  },
+} );
+</script>
